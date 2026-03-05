797d7fa71a173187e6c445dd0f48edc488ee30b4
naur-Io <ruanlucenaramos@gmail.com>
2026-03-05 15:36:11 -0300

    Feat(controller): creating TransactionController.kt with createTransaction method

---
+++ src/main/kotlin/com/itau/challenge/controllers/TransactionController.kt
@@ -0,0 +1,25 @@
+package com.itau.challenge.controllers
+
+import com.itau.challenge.models.Transaction
+import com.itau.challenge.services.TransactionService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+
+
+@RestController
+@RequestMapping("/transacao")
+class TransactionController(
+    private val transactionService: TransactionService
+) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createTransaction(@RequestBody request : Transaction) {
+        transactionService.addTransaction(request)
+    }
+
+}
