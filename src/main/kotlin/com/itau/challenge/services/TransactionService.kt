d0dc17eff68676d5ac760fc77635617c1410b2f2
naur-Io <ruanlucenaramos@gmail.com>
2026-03-05 15:36:28 -0300

    Feat(service): creating TransactionService.kt with addTransaction method

---
+++ src/main/kotlin/com/itau/challenge/services/TransactionService.kt
@@ -0,0 +1,25 @@
+package com.itau.challenge.services
+
+import com.itau.challenge.models.Transaction
+import org.springframework.stereotype.Service
+import java.time.OffsetDateTime
+import com.itau.challenge.exceptions.UnprocessableEntityException
+
+
+@Service
+class TransactionService {
+
+    fun addTransaction(transaction: Transaction) {
+
+        val currentTime = OffsetDateTime.now()
+
+        if (transaction.date.isAfter(currentTime)) {
+            throw UnprocessableEntityException("Transaction date cannot be in the future")
+        }
+
+        if (transaction.amount < 0.0) {
+            throw UnprocessableEntityException("Transaction amount must be greater than 0")
+        }
+    }
+
+}
