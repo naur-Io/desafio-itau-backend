5cb482bfad386605e15772b2c056d4076591b364
naur-Io <ruanlucenaramos@gmail.com>
2026-03-05 16:37:14 -0300

    feat(Services): add thread-safe memory storage

--- src/main/kotlin/com/itau/challenge/services/TransactionService.kt
+++ src/main/kotlin/com/itau/challenge/services/TransactionService.kt
@@ -4,10 +4,13 @@ import com.itau.challenge.models.Transaction
 import org.springframework.stereotype.Service
 import java.time.OffsetDateTime
 import com.itau.challenge.exceptions.UnprocessableEntityException
+import java.util.concurrent.CopyOnWriteArrayList
 
 
 @Service
 class TransactionService {
+
+    private val transactions = CopyOnWriteArrayList<Transaction>()
 
     fun addTransaction(transaction: Transaction) {
 
@@ -20,6 +23,13 @@ class TransactionService {
         if (transaction.amount < 0.0) {
             throw UnprocessableEntityException("Transaction amount must be greater than 0")
         }
+
+        transactions.add(transaction)
+    }
+
+
+    fun clearTransactions() {
+        transactions.clear()
     }
 
 }
