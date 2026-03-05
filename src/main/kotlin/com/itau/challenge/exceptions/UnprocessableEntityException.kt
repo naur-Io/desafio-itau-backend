fbc7e6fa2c04889db0a0a434790b4f7344979d55
naur-Io <ruanlucenaramos@gmail.com>
2026-03-05 15:37:19 -0300

    Feat(Exception): implementing UnprocessableEntityException to throw and handle erros by transaction service

---
+++ src/main/kotlin/com/itau/challenge/exceptions/UnprocessableEntityException.kt
@@ -0,0 +1,7 @@
+package com.itau.challenge.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+class UnprocessableEntityException(message: String) : RuntimeException(message)
