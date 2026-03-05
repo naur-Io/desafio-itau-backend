7561d06901ef085849a46e12e40ddd4ef867ab7d
naur-Io <ruanlucenaramos@gmail.com>
2026-03-05 17:24:23 -0300

    feat(Services): StatisticService with injection dependecy and getStatistic fun

---
+++ src/main/kotlin/com/itau/challenge/services/StatisticService.kt
@@ -0,0 +1,49 @@
+package com.itau.challenge.services
+
+import com.itau.challenge.models.Statistic
+import com.itau.challenge.models.Transaction
+import org.springframework.stereotype.Service
+import java.time.OffsetDateTime
+import java.util.concurrent.CopyOnWriteArrayList
+
+
+@Service
+class StatisticService {
+
+    private val transactions = CopyOnWriteArrayList<Transaction>()
+
+
+    private val transactionService: TransactionService
+
+    constructor(transactionService: TransactionService) {
+        this.transactionService = transactionService
+    }
+
+    fun getStatistic(): Statistic {
+        val currentTime = OffsetDateTime.now().minusSeconds(60)
+        val recentTransactions = transactions.filter { it.date.isAfter(currentTime) }
+
+        val stats = recentTransactions.stream()
+            .mapToDouble { it.amount }
+            .summaryStatistics()
+
+        if (stats.count == 0L) {
+            return Statistic(
+                count = 0L,
+                sum = 0.0,
+                avg = 0.0,
+                max = 0.0,
+                min = 0.0
+            )
+
+        }
+        return Statistic(
+            count = stats.count,
+            sum = stats.sum,
+            avg = stats.average,
+            max = stats.max,
+            min = stats.min
+        )
+    }
+
+}
