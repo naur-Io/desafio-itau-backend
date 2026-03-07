f2db376c83d3bbef85ed7d53508a3b33e2a02746
naur-Io <ruanlucenaramos@gmail.com>
2026-03-07 11:56:19 -0300

    feat(observability): implement application logging
    - Add SLF4J Logger
    - Add Logger to StatisticService & TransactionService.kt to monitor calculation triggers and empty states

--- src/main/kotlin/com/itau/challenge/services/StatisticService.kt
+++ src/main/kotlin/com/itau/challenge/services/StatisticService.kt
@@ -5,13 +5,19 @@ import com.itau.challenge.models.Transaction
 import org.springframework.stereotype.Service
 import java.time.OffsetDateTime
 import java.util.concurrent.CopyOnWriteArrayList
+import org.slf4j.LoggerFactory
 
 
 @Service
 class StatisticService(
     private val transactionService: TransactionService
+
 ) {
+
+    private val log = LoggerFactory.getLogger(StatisticService::class.java)
+
     fun getStatistic(): Statistic {
+        log.info("Calculating statistics for transactions in the last 60 seconds.")
         val currentTime = OffsetDateTime.now().minusSeconds(60)
 
         val recentTransactions = transactionService.getAllTransactions().filter { it.date.isAfter(currentTime) }
@@ -19,6 +25,7 @@ class StatisticService(
         val stats = recentTransactions.stream().mapToDouble { it.amount }.summaryStatistics()
 
         if (stats.count == 0L) {
+            log.info("None transactions found in the last 60 seconds. Returning default statistics.")
             return Statistic(
                 count = 0L,
                 sum = 0.0,
@@ -28,6 +35,7 @@ class StatisticService(
             )
         }
 
+        log.info("Statistics calculated successfully" )
         return Statistic(
             count = stats.count,
             sum = stats.sum,
--- src/main/kotlin/com/itau/challenge/services/TransactionService.kt
+++ src/main/kotlin/com/itau/challenge/services/TransactionService.kt
@@ -5,30 +5,41 @@ import org.springframework.stereotype.Service
 import java.time.OffsetDateTime
 import com.itau.challenge.exceptions.UnprocessableEntityException
 import java.util.concurrent.CopyOnWriteArrayList
+import org.slf4j.LoggerFactory
 
 
 @Service
 class TransactionService {
 
+    private val log = LoggerFactory.getLogger(TransactionService::class.java)
+
     private val transactions = CopyOnWriteArrayList<Transaction>()
 
     fun addTransaction(transaction: Transaction) {
 
+
+        log.info("Receive transaction try: value={}, date={}", transaction.amount, transaction.date)
+
         val currentTime = OffsetDateTime.now()
 
         if (transaction.date.isAfter(currentTime)) {
+            log.warn("Transaction Failed: Future date detected.")
             throw UnprocessableEntityException("Transaction date cannot be in the future")
         }
 
         if (transaction.amount < 0.0) {
+            log.warn("Transaction Failed: negative value.")
             throw UnprocessableEntityException("Transaction amount must be greater than 0")
         }
 
         transactions.add(transaction)
+        log.info("Transaction added successfully! Memory: {}", transactions.size)
     }
 
     fun clearTransactions() {
+        log.info("Clearing all transactions from memory. Current count: {}", transactions.size)
         transactions.clear()
+        log.info("Memory cleared. Current transaction count: {}", transactions.size)
     }
 
     fun getAllTransactions(): List<Transaction> {
